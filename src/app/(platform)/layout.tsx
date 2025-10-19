eb9206b399c26f36b9ef0d8864bb78895f8d761a
Steel Maxima <steelmaxima21@gmail.com>
2025-10-19 10:19:23 +0100

    here ewwwwwthgqighn90bhnq0hbntrhrr

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -2,8 +2,7 @@
 
 import { ConnectWallet } from "components/web3/connect-wallet";
 import { SidebarProvider,useSidebar} from "components/ui/sidebar";
-import { ThemeProvider } from "components/providers/theme-provider";
-import { ThemeToggle } from "components/ui/theme-toogle";
+import { ThemeToggle } from "components/ui/theme_toggle";
 import { PageLoader } from "components/ui/page-loader";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
