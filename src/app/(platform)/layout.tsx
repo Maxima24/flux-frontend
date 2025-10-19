c5671da87cb1ea5d8388a49ca822ceb3a43c1e50
Steel Maxima <steelmaxima21@gmail.com>
2025-10-19 21:22:37 +0100

    test drive 45

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/shared/appsidebar";
+
 export default function PlatformLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <div className="flex h-screen w-full">
+        <AppSidebar />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          {children}
+        </div>
+      </div>
+    </SidebarProvider>
+  );
 }
